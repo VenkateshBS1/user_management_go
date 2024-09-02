72d38b4913fb6c81270734a9741c74ba4dbe2472
VenkateshBS1 <venkateshbs758@gmail.com>
2024-09-02 16:46:46 +0530

    initial commit
    Added all the files

---
+++ golang_user_management_jwt/routes/authRouter.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	controller "github.com/venkateshBS1/golang_user_management_jwt/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("users/signup", controller.Signup())
+	incomingRoutes.POST("users/login", controller.Login())
+
+}
